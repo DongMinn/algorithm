da2d7951fb951be750499794b0d90ec2cd78065b
YoonDongmin <ddong4753@gmail.com>
2017-11-05 16:30:59 +0900

    1105 프로젝트생성

---
+++ src/InterestingParty.java
@@ -0,0 +1,11 @@
+
+public class InterestingParty {
+	public int bestInvitation(String[] first , String[] second) {
+		int i , j , ans = 0;
+		
+		for(i = 0  ; i<first.length; i++) {
+			
+		}
+		return ans;
+	}
+}
---
+++ src/KiwiProblem.java
@@ -0,0 +1,26 @@
+
+public class KiwiProblem {
+	public int [] thePouring(int[] capacities , int[] bottles , int[] fromId , int[] toId) {
+		
+		for(int i =0 ; i<fromId.length; i++) {
+			int f=  fromId[i];
+			int t = toId[i];
+			
+			int space = capacities[t] - bottles[t];
+			
+			if(space >= bottles[f]) {
+				int vol = bottles[f];
+				bottles[t]+=vol;
+				bottles[f]=0;
+				
+			}else{
+				int vol = space;
+				bottles[t]+=vol;
+				bottles[t]-=vol;
+			}
+		}
+		
+		return bottles;
+	}
+
+}
