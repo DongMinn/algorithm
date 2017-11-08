2e6810ee15a9255814d70f7d2e2ab2f239468ca6
YoonDongmin <ddong4753@gmail.com>
2017-11-08 23:59:59 +0900

    add no5_5

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import chapter5.Cryptography;
+import chapter5.FriendScore;
 import chapter5.InterestingParty;
 
 public class Main {
@@ -7,6 +8,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		No5_1();
 		No5_2();
+		No5_5();
 		
 	}
 	public static void No5_1() {
@@ -17,7 +19,7 @@ public class Main {
 		
 		int result = no2.bestInvitation(f, s);
 		
-		System.out.println("5_1¹ø¹®Á¦:"+result);
+		System.out.println("5_1ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½:"+result);
 	}
 	
 	public static void No5_2() {
@@ -25,7 +27,15 @@ public class Main {
 		int s [] = {1,1,3};
 		long result = no3.encrypth(s);
 		
-		System.out.println("5_2¹ø¹®Á¦:"+result);
+		System.out.println("5_2ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½:"+result);
+	}
+	public static void No5_5() {
+		FriendScore no5 = new FriendScore();
+		String s [] = {"NYNNN" , "YNYNN" , "NYNYN","NNYNY","NNNYN"};
+		
+		int result = no5.highestScore(s);
+		
+		System.out.println("No5: "+ result);
 	}
 
 }
